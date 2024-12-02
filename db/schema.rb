888fe9986c2241952060f2d2e5e8eb2effcdf463
Johann Speer <johann.speer@proton.me>
2024-12-02 15:05:28 -0500

    Transition to rails 6 and bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema.define(version: 0) do
+
 end
